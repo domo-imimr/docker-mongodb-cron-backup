b27ef30cfa2ff1c77cd0fc9112d7fdcf83ee2de1
domo-imimr <domo@imimr.net>
2023-10-17 11:17:35 +0800

    Local env example

--- cleanBackup.sh
+++ cleanBackup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 backup_files_count=$(ls -1 "$DESTINATION_PATH" | grep -c "^[0-9]\{2\}_[0-9]\{2\}_[0-9]\{4\}-[0-9]\{2\}:[0-9]\{2\}.gz")
-if ( [ "$backup_files_count" -gt 30 ] )
+if ( [ "$backup_files_count" -gt 14 ] )
 then
     oldest_backup=$(ls -1t "$DESTINATION_PATH" | grep "^[0-9]\{2\}_[0-9]\{2\}_[0-9]\{4\}-[0-9]\{2\}:[0-9]\{2\}.gz" | tail -n 1)
     rm "$DESTINATION_PATH/$oldest_backup"
